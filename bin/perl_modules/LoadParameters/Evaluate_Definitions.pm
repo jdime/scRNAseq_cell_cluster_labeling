1bd842765bb7d76d803b40e75ac5d07b0bde8bad
Javier Diaz-Mejia <34211920+jdime@users.noreply.github.com>
2019-02-19 21:53:21 -0500

    Add files via upload

---
+++ bin/perl_modules/LoadParameters/Evaluate_Definitions.pm
@@ -0,0 +1,260 @@
+
+### Modules LoadParameters::Evaluate_Definitions and LoadParameters::Parameters
+### work together to request and evaluate parameters for Perl programs
+
+### They were inspired by libraries "argparse" from Python and "optparse" from R
+
+### Module LoadParameters::Evaluate_Definitions evaluates that one-command-line parameters provided by the User
+### meet the expected format (numeric, integer, strings, <comma> separated options, etc)
+
+package LoadParameters::Evaluate_Definitions;
+require Exporter;
+require AutoLoader;
+
+@ISA = qw( Exporter AutoLoader );
+@EXPORT = qw(
+Evaluate_parameter_values_series_of_digits
+Evaluate_numb_cols
+Evaluate_numb_rows
+Evaluate_cutoffs_negative
+Evaluate_cutoffs_positive
+Evaluate_restrict_classes
+Evaluate_value
+Evaluate_file_exist
+Evaluate_inflation
+Evaluate_type_of_tips
+Evaluate_horizontal_offset
+%hashColumnKeyToColumnNumber
+%hash_parameter_values_to_include
+%hash_numb_cols
+%hash_numb_rows
+%hash_outfile_options
+%hash_step_numbers_to_run
+%hash_numb_rounds
+%hash_rank_classes_to_include
+%hash_cutoffs_numeric
+%hash_cutoffs_negative
+%hash_cutoffs_positive
+%hash_cutoffs_negative_or_positive
+%hashInflations
+%hash_distance_measures
+%hash_clustering_methods
+$TypeOfTipForRobot_Label
+$Horizontal_Offset
+);
+
+######################################################
+### If you ADD MORE subroutines to this module you'll need to pass input via LoadParameters::Parameters.pm
+### Also make sure to add their main output (for external code) to @EXPORT
+######################################################
+
+######################################################
+### This subroutine is for parameters whose format needs to be checked for specifit formats
+######################################################
+
+#### IMPORTANT: keep this row to START parsing of 'make_list_of_perl_dependencies_in_perl_programs.pl' script
+
+##>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+sub Evaluate_value {
+my($parameterKey,$value) = @_;
+	if ($parameterKey =~ /(^classes_type$)/) {
+		unless ($value =~ /^(gmt|profile)$/i) {
+		die "ERROR!!! Reading parameter '$parameterKey' value '$value' don't match expected format\n";
+		}
+
+	}elsif ($parameterKey =~ /(^cutoff_neg$)/) {
+	### Negative values or NA or DEFAULT
+		unless ($value =~ /(^-\d+$)|(^-\d+\.\d+$)|(^NA$)|(^default$)/i) {
+		die "ERROR!!! Reading parameter '$parameterKey' value '$value' don't match expected format\n";
+		}
+	
+
+	}elsif ($parameterKey =~ /(^cutoff_ora$)/) {
+	### Positive values or NA or DEFAULT
+		unless ($value =~ /(^\d+$)|(^\d+\.\d+$)|(^NA$)|(^default$)/i) {
+		die "ERROR!!! Reading parameter '$parameterKey' value '$value' don't match expected format\n";
+		}
+
+
+	}elsif ($parameterKey =~ /(^cutoff_pos$)/) {
+	### Positive values or NA or DEFAULT
+		unless ($value =~ /(^\d+$)|(^\d+\.\d+$)|(^NA$)|(^default$)/i) {
+		die "ERROR!!! Reading parameter '$parameterKey' value '$value' don't match expected format\n";
+		}
+
+
+	}elsif ($parameterKey =~ /(^cutoff_print_p$)/) {
+	### Positive values or NA or DEFAULT
+		unless ($value =~ /(^\d+$)|(^\d+\.\d+$)|(^NA$)|(^default$)/i) {
+		die "ERROR!!! Reading parameter '$parameterKey' value '$value' don't match expected format\n";
+		}
+
+
+	}elsif ($parameterKey =~ /(^cutoff_print_q$)/) {
+	### Positive values or NA or DEFAULT
+		unless ($value =~ /(^\d+$)|(^\d+\.\d+$)|(^NA$)|(^default$)/i) {
+		die "ERROR!!! Reading parameter '$parameterKey' value '$value' don't match expected format\n";
+		}
+
+
+	}elsif ($parameterKey =~ /(^nperm$)/) {
+	### Positive values. Don't allow 'NA'
+		unless ($value =~ /(^\d+$)|(^\d+\.\d+$)/) {
+		die "ERROR!!! Reading parameter '$parameterKey' value '$value' don't match expected format\n";
+		}
+	
+
+	}elsif ($parameterKey =~ /(^p_correction_test$)/) {
+		unless ($value =~ /(^BY$)|(^BH$)|(^bonferroni$)/) {
+		die "ERROR!!! Reading parameter '$parameterKey' value '$value' don't match expected format\n";
+		}
+
+	}elsif ($parameterKey =~ /(^set_max$)/) {
+	### Positive values. Don't allow 'NA'
+		unless ($value =~ /(^\d+$)|(^\d+\.\d+$)/) {
+		die "ERROR!!! Reading parameter '$parameterKey' value '$value' don't match expected format\n";
+		}
+	
+
+	}elsif ($parameterKey =~ /(^set_min$)/) {
+	### Positive values. Don't allow 'NA'
+		unless ($value =~ /(^\d+$)|(^\d+\.\d+$)/) {
+		die "ERROR!!! Reading parameter '$parameterKey' value '$value' don't match expected format\n";
+		}
+	
+
+	}elsif ($parameterKey =~ /(^use_graphics_device$)/) {
+		unless ($value =~ /^(pdf|png|NA)$/i) {
+		die "ERROR!!! Reading parameter '$parameterKey' value '$value' don't match expected format\n";
+		}
+
+	}elsif ($parameterKey =~ /(^use_ora$)/) {
+		unless ($value =~ /(^p$)|(^pc$)/) {
+		die "ERROR!!! Reading parameter '$parameterKey' value '$value' don't match expected format\n";
+		}
+
+	}elsif ($parameterKey =~ /(^use_universe$)/) {
+		unless ($value =~ /(^u$)|(^i$)|(^n1$)|(^n2$)/) {
+		die "ERROR!!! Reading parameter '$parameterKey' value '$value' don't match expected format\n";
+		}
+
+	}elsif ($parameterKey =~ /(^use_values_or_ranks$)/) {
+		unless ($value =~ /^(values|ranks)$/i) {
+		die "ERROR!!! Reading parameter '$parameterKey' value '$value' don't match expected format\n";
+		}
+
+	}else{
+	die "ERROR!!! Reading parameter '$parameterKey' couldn't find evaluation parameters in LoadParameters::Evaluate_Definitions\n";
+	}
+}
+##<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+##>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+sub Evaluate_directory_exist {
+my($parameterKey,$directory) = @_;
+	if ($directory =~ /^none$/) {
+	}elsif (-d $directory) {
+	}elsif ($parameterKey =~ /(^path_outfiles)/) { ### these are directories for outputs, thus if don't exist will be created
+	`mkdir -p $directory`;
+	print "WARNING!!! created directory\n'$directory'\n";
+	}elsif ($parameterKey =~ /(^hyperG_directory$)/) { ### this is an intermediate directory, thus just look that there is a defined string
+		if ($directory =~ /\S/) {
+		}else{
+		die "ERROR!!! directory '$directory' was recognized as a string\n";
+		}
+	}else{
+	die "ERROR!!! directory '$directory' was not found\n";
+	}
+}
+
+
+##<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+##>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+sub Evaluate_file_exist {
+my($parameterKey,$file) = @_;
+	if ($file =~ /(^none$)|(^na$)/i) {
+	}elsif (-f $file) {
+	}else{
+	die "ERROR!!! Reading parameter '$parameterKey' couldn't find file '$file'\n";
+	}
+}
+
+##<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+##>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+sub Evaluate_numb_cols {
+my($parameterKey,$numb_cols) = @_;
+
+	if ($parameterKey =~ /cols|column/) {
+	$optionForOutputToScreens = "columns";
+	}else{
+	$optionForOutputToScreens = "options";
+	}
+
+	if ($numb_cols =~ /(\d+)/) {
+	chomp $numb_cols;
+		
+	### here indexing by $parameterKey in case multiple calls of 'column_*' are made
+		if ($parameterKey =~ /^column_/) {
+		$hashColumnKeyToColumnNumber{$parameterKey} = $numb_cols;
+		}
+
+	@Cols = split (",", $numb_cols);
+		foreach $cc (@Cols) {
+			if ($cc =~ /^\d+$/) {
+			$hash_numb_cols{$cc} = 1;
+			}elsif ($cc =~ /^(\d+)(-)(\d+)$/) {
+			$start = $1;
+			$end = $3;
+				foreach $c ($start..$end) {
+				$hash_numb_cols{$c} = 1;
+				}
+			}else{
+			die "ERROR!!! Reading parameter '$parameterKey'  couldn't determine $optionForOutputToScreens to process from '$cc'\n";
+			}
+		}
+
+	print "The following $optionForOutputToScreens will be processed:\n";
+
+		foreach $c (sort { $a <=> $b } keys %hash_numb_cols) {
+		print "$c\n";
+		}
+
+	}elsif ($numb_cols =~ /^(ALL)$/i) {
+	print "All $optionForOutputToScreens will be processed\n";
+	}elsif ($numb_cols =~ /^(NA)$/i) {
+	}else{
+	die "ERROR!!! Reading parameter '$parameterKey' couldn't determine $optionForOutputToScreens to process from '$numb_cols'\n";
+	}
+}
+
+##<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+##>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+sub Evaluate_restrict_classes {
+my($parameterKey,$file_or_all) = @_;
+	if ($file_or_all =~ /^all$/i) {
+	}elsif (-f $file_or_all) {
+	}else{
+	die "ERROR!!! Reading parameter '$parameterKey' couldn't find file '$file_or_all' to restrict classes and no 'ALL' command was readed\n";
+	}
+}
+
+##<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+##>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+sub Evaluate_were_defined {
+my($parameterKey,$value) = @_;
+
+	unless ($value =~ /\S/) {
+	die "ERROR!!! Reading parameter '$parameterKey' string '$value' is not valid\n";
+	}
+}
+
+##<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+#### IMPORTANT: keep this row to END parsing of 'make_list_of_perl_dependencies_in_perl_programs.pl' script
+
+1;
+
---
+++ bin/perl_modules/LoadParameters/Parameters.pm
@@ -0,0 +1,209 @@
+
+### Modules LoadParameters::Evaluate_Definitions and LoadParameters::Parameters
+### work together to request and evaluate parameters for Perl programs
+
+### They were inspired by libraries "argparse" from Python and "optparse" from R
+
+### Module LoadParameters::Parameters sorts the parameter and sends it to the corresponding evaluates that one-command-line parameters provided by the User
+### meet the expected format (numeric, integer, strings, <comma> separated options, etc)
+
+package LoadParameters::Parameters;
+require Exporter;
+require AutoLoader;
+
+use LoadParameters::Evaluate_Definitions;
+use PathsDefinition::PathsToInputs;
+
+### NOTE: hashes generated in LoadParameters::Evaluate_Definitions need to be #EXPORTed here too to be used by Perl programs
+
+@ISA = qw( Exporter AutoLoader );
+@EXPORT = qw(
+MainSubParameters
+%hashParameters
+$MoreParameters
+%hashColumnKeyToColumnNumber
+%hash_parameter_values_to_include
+%hash_numb_cols
+%hash_numb_rows
+%hash_outfile_options
+%hash_step_numbers_to_run
+%hash_numb_rounds
+%hash_rank_classes_to_include
+%hash_cutoffs_numeric
+%hash_cutoffs_negative
+%hash_cutoffs_positive
+%hash_cutoffs_negative_or_positive
+%hashInflations
+%hash_distance_measures
+%hash_clustering_methods
+$TypeOfTipForRobot_Label
+$Horizontal_Offset
+);
+
+sub MainSubParameters {
+
+#### Loading inputted parameters
+
+my($In_hashParametersTolookFor,$In_arrayInputtedOneLineCommands) = @_;
+%hashParametersTolookFor = %{$In_hashParametersTolookFor};   ## here assigning the incoming hash (element 0 from @_);
+@InputtedOneLineCommands = @{$In_arrayInputtedOneLineCommands}; ## here assigning the incoming array (element 1 from @_);
+
+#### Indexing parameters
+	
+	$c = 0;
+	$CountMissingParamaters = 0;
+	foreach $parameter (@InputtedOneLineCommands) {
+	$c++;
+	
+		if ($parameter =~ /^-/) {
+			if ($parameter =~ /^-\d+/) { ## this is because a parameter values like '-aggrv_cutoff -2' might be considered as two parameters because the '-'
+			$MoreParameters .= "$parameter\n";
+			}else{
+			$MoreParameters .= "$parameter \t";
+			}
+		}else{
+		$MoreParameters .= "$parameter\n";
+		}
+	
+		if ($parameter =~ /^(-)(\S+)$/) {
+		$parameterKey = $2;
+		$parameterValue = @InputtedOneLineCommands[$c];
+			if ($parameterValue =~ /(^-*0+$)/) {
+			$parameterValue = "0.0";
+			}
+		$hashParameters{$parameterKey} = "$parameterValue";
+		}
+	}
+	
+#### Checking parameters for further details
+	
+	foreach $parameterKey (sort keys %hashParametersTolookFor) {
+		if ($hashParameters{$parameterKey}) {
+		$parameterValue = $hashParameters{$parameterKey};
+	
+		#### These are parameters which need further processing
+		#### Also you need to define such details in module LoadParameters::EvaluateDefinitions
+		#### Also you need to return any useful hash, array, etc from such other modules by @EXPORT
+		
+		#### IMPORTANT: keep this row to START parsing of 'make_list_of_perl_dependencies_in_perl_programs.pl' script
+
+			if ($parameterKey =~ /(^infile)/) {
+			### these are parameters which only specified path/files do exist
+				### This is to allow Evaluate_Definitions::Evaluate_file_exist use '-f' when the input starts with '~/' instead the root/home/Users path
+				if ($parameterValue =~ /^~\//) {
+				$PathPrefix = "/$Users_home/$DefaultUserName/";
+				$parameterValue =~ s/^~\//$PathPrefix/;
+				$hashParameters{$parameterKey} = $parameterValue;
+				}
+			LoadParameters::Evaluate_Definitions::Evaluate_file_exist($parameterKey,$parameterValue);
+
+			}elsif ($parameterKey =~ /(^prefix)/) {
+			### these are parameters which only need to check that are defined 
+			LoadParameters::Evaluate_Definitions::Evaluate_were_defined($parameterKey,$parameterValue);
+			}elsif ($parameterKey =~ /(^classes_type$)/) {
+			LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey,$parameterValue);
+			}elsif ($parameterKey =~ /(^cutoff_neg$)/) {
+			LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey,$parameterValue);
+			}elsif ($parameterKey =~ /(^cutoff_ora$)/) {
+			LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey,$parameterValue);
+			}elsif ($parameterKey =~ /(^cutoff_pos$)/) {
+			LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey,$parameterValue);
+			}elsif ($parameterKey =~ /(^cutoff_print_p$)/) {
+			LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey,$parameterValue);
+			}elsif ($parameterKey =~ /(^cutoff_print_q$)/) {
+			LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey,$parameterValue);
+			}elsif ($parameterKey =~ /(^nperm$)/) {
+			LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey,$parameterValue);
+			}elsif ($parameterKey =~ /(^numb_cols$)/) {
+			LoadParameters::Evaluate_Definitions::Evaluate_numb_cols($parameterKey,$parameterValue);
+			}elsif ($parameterKey =~ /(^p_correction_test$)/) {
+			LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey,$parameterValue);
+			}elsif ($parameterKey =~ /(^path_outfiles$)/) {
+			### these are parameters which only need specified path/directories do exist
+			$hashParameters{$parameterKey} =~ s/\/$//;
+			LoadParameters::Evaluate_Definitions::Evaluate_directory_exist($parameterKey,$parameterValue);
+			}elsif ($parameterKey =~ /(^restrict_classes$)/) {
+			LoadParameters::Evaluate_Definitions::Evaluate_restrict_classes($parameterKey,$parameterValue);
+			}elsif ($parameterKey =~ /(^set_max$)/) {
+			### these are parameters whose values format needs to be checked and make sure Min <= Max value
+			LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey,$parameterValue);
+				if ($parameterKey =~ /(^set_min$)/) {
+					if ($hashParameters{set_max}) {
+					$parameterKey_set_max = "set_max";
+					$parameterValue_set_max = $hashParameters{set_max};
+					LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey_set_max,$parameterValue_set_max);
+						unless ($parameterValue <= $parameterValue_set_max) {
+						die "ERROR!!! unexpected comparison '$parameterKey' = '$parameterValue' and '$parameterKey_set_max' = '$parameterValue_set_max', but they are expected '$parameterKey' <= '$parameterKey_set_max'\n";
+						}
+					}
+				}elsif ($parameterKey =~ /(^set_max$)/) {
+					if ($hashParameters{set_min}) {
+					$parameterKey_set_min = "set_min";
+					$parameterValue_set_min = $hashParameters{set_min};
+					LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey_set_min,$parameterValue_set_min);
+						unless ($parameterValue >= $parameterValue_set_min) {
+						die "ERROR!!! unexpected comparison '$parameterKey' = '$parameterValue' and '$parameterKey_set_min' = '$parameterValue_set_min', but they are expected '$parameterKey' >= '$parameterKey_set_min'\n";
+						}
+					}
+				}
+
+			}elsif ($parameterKey =~ /(^set_min$)/) {
+			### these are parameters whose values format needs to be checked and make sure Min <= Max value
+			LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey,$parameterValue);
+				if ($parameterKey =~ /(^set_min$)/) {
+					if ($hashParameters{set_max}) {
+					$parameterKey_set_max = "set_max";
+					$parameterValue_set_max = $hashParameters{set_max};
+					LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey_set_max,$parameterValue_set_max);
+						unless ($parameterValue <= $parameterValue_set_max) {
+						die "ERROR!!! unexpected comparison '$parameterKey' = '$parameterValue' and '$parameterKey_set_max' = '$parameterValue_set_max', but they are expected '$parameterKey' <= '$parameterKey_set_max'\n";
+						}
+					}
+				}elsif ($parameterKey =~ /(^set_max$)/) {
+					if ($hashParameters{set_min}) {
+					$parameterKey_set_min = "set_min";
+					$parameterValue_set_min = $hashParameters{set_min};
+					LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey_set_min,$parameterValue_set_min);
+						unless ($parameterValue >= $parameterValue_set_min) {
+						die "ERROR!!! unexpected comparison '$parameterKey' = '$parameterValue' and '$parameterKey_set_min' = '$parameterValue_set_min', but they are expected '$parameterKey' >= '$parameterKey_set_min'\n";
+						}
+					}
+				}
+
+			}elsif ($parameterKey =~ /(^use_graphics_device$)/) {
+			LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey,$parameterValue);
+			}elsif ($parameterKey =~ /(^use_ora$)/) {
+			LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey,$parameterValue);
+			}elsif ($parameterKey =~ /(^use_universe$)/) {
+			LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey,$parameterValue);
+			}elsif ($parameterKey =~ /(^use_values_or_ranks$)/) {
+			LoadParameters::Evaluate_Definitions::Evaluate_value($parameterKey,$parameterValue);
+
+		#### IMPORTANT: keep this row to END parsing of 'make_list_of_perl_dependencies_in_perl_programs.pl' script
+
+			}else{
+			die "\nERROR!!! parameter '$parameterKey' is not definied in module LoadParameters::Parameters\n";
+			}
+
+		}else{
+		$hashMissingParameters{$parameterKey} = 1;
+		$CountMissingParamaters++;
+		}
+	}
+	
+#### Checking if all requested parameters were defined
+	
+	if ($CountMissingParamaters > 0) {
+	print "\nThe following one-line-command parameters are needed, please provide them and try again:\n";
+		foreach $parameter (sort keys %hashMissingParameters) {
+		print "-$parameter\n";
+		}
+	die "\nThis program is exiting now by module LoadParameters::Parameters\n\n";
+	}else{
+	print "\nAll parameters needed were succesfully loaded\n";
+	}
+
+}
+
+1;
+
